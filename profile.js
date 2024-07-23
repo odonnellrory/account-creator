63323f02260f342d40f745daf285960ae0f7f407
odonnellrory <roryod.134@gmail.com>
2024-07-23 17:09:20 +0100

    massively improved css

--- profile.js
+++ profile.js
@@ -1,19 +1,20 @@
-const urlParams = new URLSearchParams(window.location.search)
+const urlParams = new URLSearchParams(window.location.search);
 const firstName = urlParams.get("firstName");
 const lastName = urlParams.get("lastName");
 const username = urlParams.get("username");
 const avatarImageURL = urlParams.get("avatarImage");
 const email = urlParams.get("email");
 
+const firstNameInput = document.getElementById("firstNameInput");
+firstNameInput.innerText = firstName;
+const lastNameInput = document.getElementById("lastNameInput");
+lastNameInput.innerText = lastName;
+const usernameInput = document.getElementById("usernameInput");
+usernameInput.innerText = username;
+const emailInput = document.getElementById("emailInput");
+emailInput.innerText = email;
 
-const firstNameInput =document.getElementById("firstNameInput")
-firstNameInput.innerText=firstName
-const lastNameInput =document.getElementById("lastNameInput")
-lastNameInput.innerText=lastName
-const usernameInput =document.getElementById("usernameInput")
-usernameInput.innerText=username
-const emailInput =document.getElementById("emailInput")
-emailInput.innerText=email
-
-const avatarInput =document.getElementById("avatarInput")
-avatarInput.src=avatarImageURL
+const avatarInput = document.getElementById("avatarInput");
+if (avatarUrl) {
+  avatarInput.src = avatarImageURL;
+}
